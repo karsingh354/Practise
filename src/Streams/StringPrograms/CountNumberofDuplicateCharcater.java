aa694b693c5c96a29592e8a291ebe33d6ce69abb
karsingh354 <61073933+karsingh354@users.noreply.github.com>
2025-01-24 04:25:56 +0530

    Update CountNumberofDuplicateCharcater.java
    LogicUpldated

--- src/Streams/StringPrograms/CountNumberofDuplicateCharcater.java
+++ src/Streams/StringPrograms/CountNumberofDuplicateCharcater.java
@@ -2,25 +2,33 @@ package Streams.StringPrograms;
 
 public class CountNumberofDuplicateCharcater {
     public static void main(String[] args) {
-        String string="I am developer";
-        char[] cstring=string.toCharArray();
-        int count=0;
-        for(int i=0;i< cstring.length-1;i++)
-        {
-             count=1;
+      
+            String string = "I am developer";
+        char[] cstring = string.toCharArray();
 
-             for(int j=i+1;j< cstring.length-1;j++)
-             {
-                 if(cstring[i]==cstring[j])
-                 {
-                     count++;
-                 }
-             }
-            if(count>1&& cstring[i]!='0')
-            {
-                System.out.println(count+" "+cstring[i]);
+        // Iterate through the string
+        for (int i = 0; i < cstring.length; i++) {
+            // Skip the space or already counted characters (those marked as '0')
+            if (cstring[i] == ' ' || cstring[i] == '0') {
+                continue;
+            }
+
+            int count = 1;
+
+            // Compare current character with the rest of the string
+            for (int j = i + 1; j < cstring.length; j++) {
+                if (cstring[i] == cstring[j]) {
+                    count++;
+                    cstring[j] = '0'; // Mark it as '0' so it won't be counted again
+                }
+            }
+
+            // If count is more than 1, print the character and its count
+            if (count > 1) {
+                System.out.println(count + " " + cstring[i]);
             }
         }
+
 
     }
 }
