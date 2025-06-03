e6df17df731abbae77393ac55f2b553856c0acf9
karsingh354 <karsingh354@gmail.com>
2025-06-03 12:06:42 +0530

    FindGroupOfAnagran
    FindGroupOfAnagran

---
+++ src/Streams/LeetCodeQuestion/FindGroupOfAnagran.java
@@ -0,0 +1,22 @@
+package Streams.LeetCodeQuestion;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FindGroupOfAnagran {
+    public static void main(String[] args) {
+        //Finf the Group Of Anagram
+        List<String> lst= Arrays.asList("cat","tac","rat","atr");
+        Collection<List<String>> groupedAnagram=lst.stream().collect(Collectors.groupingBy(i->{
+                                 char[] chars=i.toCharArray();
+                                 Arrays.sort(chars);
+                                 return new String(chars);
+                          })).values();
+        System.out.println("Group of Anagrams are given"+groupedAnagram);
+        //Find the only anagrams in List
+        List<String> strAnagram= groupedAnagram.stream().filter(i->i.size()>1).flatMap(List::stream).collect(Collectors.toList());
+        System.out.println(strAnagram);
+    }
+}
