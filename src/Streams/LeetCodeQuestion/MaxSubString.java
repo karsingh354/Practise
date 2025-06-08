e97e7ddfca2111e424aeeb974562d8deecc9bbd7
karsingh354 <karsingh354@gmail.com>
2025-06-08 14:38:20 +0530

    MaxSubStringLeetCodeQuestion
    MaxSubStringLeetCodeQuestion

---
+++ src/Streams/LeetCodeQuestion/MaxSubString.java
@@ -0,0 +1,28 @@
+package Streams.LeetCodeQuestion;
+
+import java.util.HashSet;
+import java.util.Set;
+//Find the length of Max sub String 
+public class MaxSubString {
+    public static void main(String[] args) {
+        String s="acdcgdjcdgsfacdac";
+        int right=0;
+        int left=0;
+        int max=0;
+        Set<Character> st=new HashSet<>();  //Set Always store Unique Data
+
+        while (right < s.length())
+        {
+            if(st.add(s.charAt(right)))     //Adding unique Value
+            {
+                max=Math.max(max,right-left+1); //Check the Count
+                right++;
+            }
+            else {
+                st.remove(s.charAt(left));    //Remove value if they found Repeated
+                left++;                       //Move on next chararcter
+            }
+        }
+        System.out.println(max);
+    }
+}
